d66d00e6bb02b19287231ad1feae387d900515d0
Baptiste-Caral <baptiste.caral@gmail.com>
2020-04-05 21:09:11 +0200

    DELETE + Router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const {success, error} = require('./functions'); // success() & error() created 
 const express = require('express'); // framework Back-end JS
 const morgan = require('morgan'); // debug tool
 const app = express();
+const config = require('./config')
 
 // Tableau des membres (Provisoire)
 const members = [
@@ -18,83 +19,142 @@ const members = [
         name: 'Flash'
     },
 ]
+let MembersRouter = express.Router() // /api/v1/members
+
 
 app.use(morgan('dev')) // debug tool => requests displaying in console (dev only)
 app.use(express.json()) // for parsing application/json https://expressjs.com/fr/4x/api.html#req.body:
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-// RECUPERE UN MEMBRE AVEC SON ID - READ
-app.get('/api/v1/members/:id', (req, res) => {
+MembersRouter.route('/:id')
 
-    let index = getIndex(req.params.id) // getIndex(id) sert à voir si l'ID existe
+    // GET A MEMBER WITH IS ID - READ
+    .get((req, res) => {
 
-    if (typeof(index) == 'string') { // si c'est une string (Wrong Id) -> l'id n'existe pas
-        res.json(error(index))
-    } else {
-        res.json(success(members[index])) // renvoi le membre en fonction de son id 'req.params.id'
-    }
-    
-})
-// RECUPERE TOUS LES MEMBRES - READ
-app.get('/api/v1/members/', (req, res) => {
-    // affiche le nombre de membre demandé dans l'url avec le param ?max=
-    // /api/v1/members?max=2 affiche les 2 premiers membres 
-    if (req.query.max != undefined && req.query.max > 0) {
-        res.json(success(members.slice(0, req.query.max)))
+        let index = getIndex(req.params.id) // getIndex(id) sert à voir si l'ID existe
 
-    }
-    else if (req.query.max != undefined) {
-        res.json(error('Wrong max value'))
-
-    }
-    // sinon => affiche TOUS les Membres
-        else {
-            res.json(success(members)) //  success() créee dans functions.js
+        if (typeof(index) == 'string') { // si c'est une string (Wrong Id) -> l'id n'existe pas
+            res.json(error(index))
+        } else {
+            res.json(success(members[index])) // renvoi le membre en fonction de son id 'req.params.id'
         }
+        
+    })
+    // MODIFY A MEMBER - UPDATE
+
+    .put((req,res) => {
+
+        // -- On teste si l'id existe:
+
+        let index = getIndex(req.params.id) // getIndex(id) sert à récuperer l'ID
+
+        if (typeof(index) == 'string') { // si c'est une string (Wrong Id) -> l'id n'existe pas
+            res.json(error(index))
+
+        
+        } else {
+            // -- On teste si le non (name) n'est pas déjà utilisé
+
+            let same = false
+
+            for (let i = 0; i < members.length; i++) {
+                if (req.body.name == members[i].name && req.params.id != members[i].id ) {// nom dejà utilisé && même id=>(pour éviter bug si on remplace par le meme nom actuel)   
+
+                    same = true //l'erreur dans le if ci-dessous
+                    break
+                }
+            }
+            if (same) {
+
+                res.json(error('same name'))
     
-    
-    res.send(members)
-})
-// MODIFICATION D'UN MEMBRE - UPDATE
+            } else {
 
-// AJOUT D'UN NOUVEAU MEMBRE - CREATE
-app.post('/api/v1/members', (req, res) => {
-    // req.body.name = nom inseré par l'user
-
-    if (req.body.name) {
-
-        let sameName = false // on initialise sameName à false
-        // VERIFICATION
-        // on verifie d'abord avec le for si le nom existe, si oui =>  on passe sameName à true
-        for (let i =0; i < members.length; i++) {
-            if (members[i].name == req.body.name) {
-                sameName = true // sameName devient true, ça va servir plus bas pour renvoyer l 'erreur
-                break
+            // -- On modifie le nom  
+                members[index].name = req.body.name
+                res.json(success(true)) // renvoi status: success lorsque le nom a bien été modifié
             }
         }
-        // ERREUR
-        if (sameName) {// si sameName = true (c'est le cas si dans la boucle au dessus name existe dèjà)
-            res.json(error('name already taken')) // alors on renvoi une erreur
-            
-        } else{
-        // AJOUT
-            
-            let member = {
-                id: members.length+1,
-                name: req.body.name
-            }
-            members.push(member) // ajoute l'objet member (id+name) dans le tableau members
-            res.json(success(member)) // renvoi en réponse le nouveau membre 'member'
+    })
+
+    // DELETE A MEMBER - DELETE
+
+    .delete((req, res) => {
+        
+        let index = getIndex(req.params.id);
+
+        if (typeof(index) == 'string') {
+            res.json(error(index))
+        } else {
+            members.splice(index, 1)
+            res.json(success(members))
         }
-    } else {
-        res.json(error('no name value')) // renvoi une erreur si 'req.body.name' est false
-    }
-})
+    })
+
+MembersRouter.route('/') // => /api/v1/members
+ 
+    // GET ALL MEMBERS - READ
+    .get((req, res) => {
+        // affiche le nombre de membre demandé dans l'url avec le param ?max=
+        // /api/v1/members?max=2 affiche les 2 premiers membres 
+        if (req.query.max != undefined && req.query.max > 0) {
+            res.json(success(members.slice(0, req.query.max)))
+
+        } else if (req.query.max != undefined) {
+            res.json(error('Wrong max value'))
+          
+        } else {
+           // affiche TOUS les Membres
+                res.json(success(members)) //  success() => functions.js folder
+            }
+    })
+
+    // ADD A NEW MEMBER - CREATE
+
+    .post((req, res) => {
+        // req.body.name = nom inseré par l'user
+
+        if (req.body.name) {
+
+            let sameName = false // on initialise sameName à false
+            // Vérify
+            // on verifie d'abord avec le for si le nom existe, si oui =>  on passe sameName à true
+            for (let i =0; i < members.length; i++) {
+                if (members[i].name == req.body.name) {
+                    sameName = true // sameName devient true, ça va servir plus bas pour renvoyer l 'erreur
+                    break
+                }
+            }
+            // Error
+            if (sameName) {// si sameName = true (c'est le cas si dans la boucle au dessus name existe dèjà)
+                res.json(error('name already taken')) // alors on renvoi une erreur
+                
+            } else{
+            // ADD
+                
+                let member = {
+                    id: createId(), 
+                    name: req.body.name
+                }
+                members.push(member) // ajoute l'objet member (id+name) dans le tableau members
+                res.json(success(member)) // renvoi en réponse le nouveau membre 'member'
+                
+            }
+        } else {
+            res.json(error('no name value')) // renvoi une erreur si 'req.body.name' est false
+        }
+    })
+
+// CREATION ROUTE MEMBERS
+// rootAPI is in config.json file
+app.use(config.rootAPI+'members', MembersRouter) // MembersRouter = /api/v1/members    
+
 // APP WORKS ON PORT 8080
-app.listen(8080, () => {
-    console.log('Started on port 8080');  
+
+app.listen(config.port, () => {
+    console.log('Started on port '+config.port);  
 })
-// 
+
 function getIndex(id) {
     for (let i = 0; i < members.length; i++) {
         if (members[i].id == id) {
@@ -102,4 +162,10 @@ function getIndex(id) {
         }   
     }
     return 'Wrong id'
+}
+// Crée une Id en fonction du dernier membre ayant l'id le plus elevé + 1
+// (au cas ou un membre aurait été supprimé)
+function createId() {
+    
+    return members[members.length-1].id + 1 
 }
